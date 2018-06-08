c54e385fa3eed88f9df0d1828ed55d6bd5d1ad47
Karamel <karamel@creativekara.fr>
2018-06-08 13:18:41 +0200

    Replace deprecated minetest.setting_* by minetest.settings methods.

--- init.lua
+++ init.lua
@@ -18,43 +18,43 @@ end
 
 -- Set functions to get configuration and default values
 function natural_slopes.setting_enable_surface_update()
-	local value = minetest.setting_getbool('natural_slopes.enable_surface_update')
+	local value = minetest.settings:get_bool('natural_slopes.enable_surface_update')
 	if value == nil then return true end
 	return value
 end
 function natural_slopes.setting_surface_update_interval()
-	return tonumber(minetest.setting_get('natural_slopes.surface_update_interval')) or 5.0
+	return tonumber(minetest.settings:get('natural_slopes.surface_update_interval')) or 5.0
 end
 function natural_slopes.setting_enable_shape_on_walk()
 	if not _G.poschangelib then return false end
-	local value = minetest.setting_getbool('natural_slopes.enable_shape_on_walk')
+	local value = minetest.settings:get_bool('natural_slopes.enable_shape_on_walk')
 	if value == nil then return true end
 	return value
 end
 function natural_slopes.setting_register_default_nodes()
-	local value = minetest.setting_getbool('natural_slopes.register_default_slopes')
+	local value = minetest.settings:get_bool('natural_slopes.register_default_slopes')
 	if value == nil then value = true end
 	if not _G.default then value = false end
 	return value
 end
 function natural_slopes.setting_enable_shape_on_generation()
-	local value = minetest.setting_getbool('natural_slopes.register_default_slopes')
+	local value = minetest.settings:get_bool('natural_slopes.register_default_slopes')
 	if value == nil then value = true end
 	return value
 end
 function natural_slopes.setting_generation_factor()
-	return tonumber(minetest.setting_get('natural_slopes.update_shape_generate_factor')) or 0.05
+	return tonumber(minetest.settings:get('natural_slopes.update_shape_generate_factor')) or 0.05
 end
 function natural_slopes.setting_generation_skip()
-	return tonumber(minetest.setting_get('natural_slopes.update_shape_generate_skip')) or 0
+	return tonumber(minetest.settings:get('natural_slopes.update_shape_generate_skip')) or 0
 end
 function natural_slopes.setting_enable_shape_on_dig_place()
-	local value = minetest.setting_getbool('natural_slopes.enable_shape_on_dig_place')
+	local value = minetest.settings:get_bool('natural_slopes.enable_shape_on_dig_place')
 	if value == nil then value = true end
 	return value
 end
 function natural_slopes.setting_smooth_rendering()
-	local value = minetest.setting_getbool('natural_slopes.smooth_rendering')
+	local value = minetest.settings:get_bool('natural_slopes.smooth_rendering')
 	if value == nil then value = false end
 	return value
 end
